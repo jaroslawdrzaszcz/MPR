8e2ddef6857a5ce205b20204c312133e27e51404
jaroslawdrzaszcz <32591153+jaroslawdrzaszcz@users.noreply.github.com>
2017-12-06 19:27:26 +0100

    Add files via upload

---
+++ MPR_jdbcdemo_34/src/main/java/jdbcdemo/jdbcdemo/App.java
@@ -0,0 +1,34 @@
+package jdbcdemo.jdbcdemo;
+
+import java.util.List;
+
+import domain.Person;
+import jdbcdemo.dao.PersonRepository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	PersonRepository repository = new PersonRepository();
+    	//repository.createTable();
+    	
+    	Person janek = new Person();
+    	janek.setName("Jan");
+    	janek.setSurname("Kowalski");
+    	janek.setAge(30);
+    	
+    	repository.add(janek);
+    	
+    	List<Person> people = repository.getAll();
+    	
+    	for(Person p : people){
+    		System.out.println(p.getId()+"\t"+p.getName()+"\t"+p.getSurname()+"\t"+p.getAge());
+    	}
+    	
+        System.out.println( "Koniec" );
+    }
+}
